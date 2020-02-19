8a68e11a46c1890e579ee5cab16da60bde9b94f0
Paul <paul.ivanov1990@gmail.com>
2020-02-19 14:53:10 +0300

    Homework 2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
         formCalculate = document.querySelector('.form-calculate'),
         endButton = document.querySelector('.end-button'),
         total = document.querySelector('.total'),
-        fastRange = document.querySelector('.fast-range');
+        fastRange = document.querySelector('.fast-range'),
+        totalPriceSum = document.querySelector('.total_price__sum');
+          
+  const DATA = {
+    whichSite: ['landing', 'multiPage', 'onlineStore'],
+    price: [4000, 8000, 26000],
+    desktopTemplates: [50, 40, 30],
+    adapt: 20,
+    mobileTemplates: 15,
+    editable: 10,
+    metrikaYandex: [500, 1000, 2000],
+    analyticsGoogle: [850, 1350, 3000],
+    sendOrder: 500,
+    deadlineDay: [[2, 7], [3, 10], [7, 14]],
+    deadlinePercent: [20, 17, 15]
+  };
 
   function showElem(elem) {
     elem.style.display = 'block';
@@ -14,11 +29,58 @@ document.addEventListener('DOMContentLoaded', function() {
   function hideElem(elem) {
     elem.style.display = 'none';
   }
+
+  function priceCalculation(elem) {
+    let result = 0,
+        index = 0,
+        options = [];
+
+    if (elem.name === 'whichSite') {
+      for (const item of formCalculate.elements) {
+        if (item.type === 'checkbox') {
+          item.checked = false;
+        }
+      }
+      hideElem(fastRange);
+    }
+
+    for (const item of formCalculate.elements) {
+      if (item.name === 'whichSite' && item.checked) {
+        index = DATA.whichSite.indexOf(item.value);
+      } else if (item.classList.contains('calc-handler') && item.checked) {
+        options.push(item.value);
+      }
+    }
+
+    options.forEach(function(key) {
+      if (typeof(DATA[key]) === 'number') {
+        if (key === 'sendOrder') {
+          result += DATA[key];
+        } else {
+          result += DATA.price[index] * DATA[key] / 100;
+        }
+      } else {
+        if (key === 'desktopTemplates') {
+          result += DATA.price[index] * DATA[key][index] / 100;
+        } else {
+          result += DATA[key][index];
+        }
+      }
+    });
+
+    result += DATA.price[index];
+
+    totalPriceSum.textContent = result;
+  }
   
   function handlerCallBackForm(event) {
     const target = event.target;
     if (target.classList.contains('want-faster')) {
       target.checked ? showElem(fastRange) : hideElem(fastRange);
+    }
+
+    if (target.classList.contains('calc-handler')) {
+      priceCalculation(target);
     }
   }
 
@@ -35,7 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     showElem(total);
   });
-
+  
   formCalculate.addEventListener('change', handlerCallBackForm);
 
 });
